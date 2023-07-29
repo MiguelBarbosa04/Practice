d65c41641470fe0d35c675676b51bac7f86ea8d0
MiguelBarbosa04 <m19b2002@gmail.com>
2023-07-29 01:11:22 +0100

    +

---
+++ PP_FP04/src/main/java/pp_fp04/exchange/CurrencyRates.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pp_fp04.exchange;
+
+/**
+ *
+ * @author Miguel
+ */
+public class CurrencyRates {
+
+    double dolar = 1.00000;
+    double euro = 1.00000;
+    double iene = 1.00000;
+double dolarToeuro = 0.76900;
+    
+
+}
